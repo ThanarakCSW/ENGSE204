343bd6b798396f33b7db0eaaedaa3188d4f2fb04
CThanarak <thanaruk.pm@gmail.com>
2025-11-21 22:55:52 +0700

    commit

--- lab1/src/lab1/lab1_1.java
+++ lab1/src/lab1/lab1_1.java
@@ -11,52 +11,28 @@ public class lab1_1 {
         boolean InputNotcorrect = true ;
 
         Scanner input = new Scanner(System.in) ;
-
-        System.out.print( "Enter Num1 : " ) ;
+        System.out.println( "Please Enter Number" ) ;
+        System.out.println( "Enter Num1 : " ) ;
         
         if( input.hasNextInt()) {
-
             num1 = input.nextInt() ;
-            
-            System.out.print( "Enter Num2 : " ) ;
+
+            System.out.println( "Enter Num2 : " ) ;
 
             if( input.hasNextInt()) {
                 num2 = input.nextInt() ;
                 InputNotcorrect = false ;
                 input.close() ;
-            }else if (input.hasNextDouble()) {
-                System.out.println("Plase Enter Just Only Number" ) ;
-                input.next() ;
-            }else if (input.hasNextFloat()){
-                System.out.println("Plase Enter Just Only Number" ) ;
-                input.next() ;
-            }else if (input.hasNextBoolean()){
-                System.out.println("Plase Enter Just Only Number" ) ;
-                input.next() ;
-            }else {
-            System.out.println("Plase Enter Just Only Number") ;
-            input.next() ;
+            }else{
+                System.out.println("Enter only Number!!!" ) ;
+            }// End if
+        }else{
+            System.out.println("Enter only Number!!!" ) ;
         }// End if
-            
-        }else if (input.hasNextDouble()) {
-            System.out.println("Plase Enter Just Only Number" ) ;
-            input.next() ;
-        }else if (input.hasNextFloat()){
-            System.out.println("Plase Enter Just Only Number" ) ;
-            input.next() ;
-        }else if (input.hasNextBoolean()){
-            System.out.println("Plase Enter Just Only Number" ) ;
-            input.next() ;
-        }else {
-            System.out.println("Plase Enter Just Only Number") ;
-            input.next() ;
-        }// End if
-
+        
         if( InputNotcorrect == false ){
             result = num1 + num2 ;
             System.out.println( "Result : " + result ) ;
         }// End if
     }// End main
 }// End class
-
-//ผมพิมพ์เองไม่ได้ใช้ AI ครับ T-T
